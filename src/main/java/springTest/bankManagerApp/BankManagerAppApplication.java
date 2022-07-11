ab9a0399f03a8fd6a5d81298213e553e101e4ab9
Bernard Ross <bernardross0763@gmail.com>
2022-07-11 12:07:37 -0400

    start of spring boot bank app

---
+++ src/main/java/springTest/bankManagerApp/BankManagerAppApplication.java
@@ -0,0 +1,13 @@
+package springTest.bankManagerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankManagerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankManagerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springTest/bankManagerApp/BankManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package springTest.bankManagerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankManagerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
